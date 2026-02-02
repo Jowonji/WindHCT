cda36f76dd79ab2ba166c7756673da94eb6635cc
JO WONJI <106738559+Jowonji@users.noreply.github.com>
2026-02-02 13:49:01 +0900

    WIndHCT

---
+++ basicsr/data/numpy_paired_dataset.py
@@ -0,0 +1,69 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from basicsr.utils.registry import DATASET_REGISTRY
+import random
+import torchvision.transforms.functional as TF
+import torchvision.transforms as transforms
+
+@DATASET_REGISTRY.register()
+class NumpyPairedDataset(Dataset):
+
+    def __init__(self, opt):
+        super(NumpyPairedDataset, self).__init__()
+        self.opt = opt
+
+        self.hr_data = np.load(opt['dataroot_gt']).astype(np.float32)
+        self.lr_data = np.load(opt['dataroot_lq']).astype(np.float32)
+
+        assert len(self.hr_data) == len(self.lr_data), "HR and LR datasets must have the same number of samples."
+        self.scale = opt['scale']
+
+        print(f"[Dataset] Loaded HR shape: {self.hr_data.shape}, LR shape: {self.lr_data.shape}")
+
+    def __getitem__(self, index):
+        hr = self.hr_data[index]
+        lr = self.lr_data[index]
+
+        if hr.ndim == 2:
+            hr = np.expand_dims(hr, axis=0)
+        if lr.ndim == 2:
+            lr = np.expand_dims(lr, axis=0)
+
+        h_gt, w_gt = hr.shape[1], hr.shape[2]
+        h_lq, w_lq = lr.shape[1], lr.shape[2]
+        computed_scale_h = h_gt / h_lq
+        computed_scale_w = w_gt / w_lq
+        epsilon = 0.05
+        self.scale = round(computed_scale_h, 2)
+        if not (abs(computed_scale_h - self.scale) < epsilon and abs(computed_scale_w - self.scale) < epsilon):
+            raise ValueError(
+                f"Scale mismatches. Computed scale ({computed_scale_h:.3f}, {computed_scale_w:.3f}) does not match expected scale {self.scale}."
+            )
+
+        hr_tensor = torch.from_numpy(hr)
+        lr_tensor = torch.from_numpy(lr)
+
+        if self.opt.get('use_hflip', False) and random.random() < 0.5:
+            hr_tensor = TF.hflip(hr_tensor)
+            lr_tensor = TF.hflip(lr_tensor)
+
+        if self.opt.get('use_rot', False):
+            angle = random.uniform(-15, 15)
+            hr_tensor = TF.rotate(hr_tensor, angle, interpolation=transforms.InterpolationMode.BILINEAR)
+            lr_tensor = TF.rotate(lr_tensor, angle, interpolation=transforms.InterpolationMode.BILINEAR)
+
+        if self.opt.get('use_blur', False) and random.random() < 0.3:
+            sigma = random.uniform(0.1, 0.5)
+            hr_tensor = TF.gaussian_blur(hr_tensor, kernel_size=3, sigma=sigma)
+            lr_tensor = TF.gaussian_blur(lr_tensor, kernel_size=3, sigma=sigma)
+
+        return {
+            'gt': hr_tensor,
+            'lq': lr_tensor,
+            'gt_path': str(index),
+            'lq_path': str(index)
+        }
+
+    def __len__(self):
+        return len(self.hr_data)
